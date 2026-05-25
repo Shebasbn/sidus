8bc73f4d27a6434ff53bd7636b78475a0233dc62
Sebastian Bautista Navarro <shebasbn@gmail.com>
2026-05-25 09:28:45 -0400

    restarted repos

--- engine/src/engine_entry.c
+++
@@ -1,8 +0,0 @@
-#include "core.h"
-#include "platform\platform.h"
-#include "application_types.h"
-
-#include "core\core_memory.c"
-#include "core\core_logger.c" 
-#include "platform\platform_win32.c"
-#include "core\core_application.c" 
